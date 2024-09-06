4e20c0c2c5b7d77c6bce2bdd1581b5739e651468
Precious <precious.soriano@ynov.com>
2024-09-06 20:29:55 +0200

    update: update user password

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Users = require('../models/userModel');
 const createUser = require('../controllers/users/createUser');
 const updateUser = require('../controllers/users/updateUser');
 const deleteUser = require('../controllers/users/deleteUser');
@@ -16,6 +17,27 @@ router.get('/', getAllUsers);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+router.put('/:id/password', async (req, res) => {
+    const { id } = req.params;
+    const { newPassword } = req.body;
+
+    try {
+      const user = await Users.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé.' });
+      }
+
+      // Hachage du nouveau mot de passe
+      user.password = newPassword;
+      user.markModified('password');  // Force la reconnaissance de la modification du mot de passe
+      await user.save();
+
+      res.status(200).json({ message: 'Mot de passe mis à jour avec succès.' });
+    } catch (error) {
+      res.status(500).json({ error: 'Erreur lors de la mise à jour du mot de passe.' });
+    }
+});
 
 // Routes pour l'authentification (sign in et sign up)
 router.post('/signin', signInUser);
