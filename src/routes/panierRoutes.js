93a07a44faa618276784d3db658ce1f8269c9ea2
Precious <precious.soriano@ynov.com>
2024-09-11 20:56:15 +0200

    fix: removed sync cart

--- src/routes/panierRoutes.js
+++ src/routes/panierRoutes.js
@@ -4,7 +4,6 @@ const updatePanier = require('../controllers/paniers/updatePanier');
 const deletePanier = require('../controllers/paniers/deletePanier');
 const getPanierById = require('../controllers/paniers/getPanierById');
 const getAllPaniers = require('../controllers/paniers/getAllPaniers');
-const syncPanier = require('../controllers/paniers/syncPanier'); 
 
 const router = express.Router();
 
@@ -14,6 +13,5 @@ router.get('/', getAllPaniers);
 router.get('/:id', getPanierById);
 router.put('/:id', updatePanier);
 router.delete('/:id', deletePanier);
-router.post('/sync', syncPanier);
 
 module.exports = router;
