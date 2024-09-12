b3bb1bcfe8053893d18eecb593abd3931d6b0108
Precious <precious.soriano@ynov.com>
2024-09-13 01:20:01 +0200

    add: delete generated image

--- src/routes/generatedImagesRoutes.js
+++ src/routes/generatedImagesRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createGeneratedImage = require('../controllers/generatedImages/createGeneratedImage');
 const getGeneratedImagesByUserId = require('../controllers/generatedImages/getGeneratedImagesByUserId');
+const deleteGeneratedImage = require('../controllers/generatedImages/deleteGeneratedImage');
 
 const router = express.Router();
 
 router.post('/', createGeneratedImage);
 router.get('/:userId', getGeneratedImagesByUserId);
+router.delete('/:imageId', deleteGeneratedImage);
 
 module.exports = router;
