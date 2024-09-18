64d233ca189ffa495bafcb1ab1e99bf53f5f4cac
Precious <precious.soriano@ynov.com>
2024-09-18 22:47:17 +0200

    fix: buy a promotionnal product

--- src/controllers/products/getProductById.js
+++ src/controllers/products/getProductById.js
@@ -10,10 +10,16 @@ const getProductById = async (req, res) => {
       return res.status(404).json({ error: 'Produit non trouvé.' });
     }
 
-    res.status(200).json(product);
+    res.status(200).json({
+      ...product._doc,
+      price: product.discountPrice ? product.discountPrice : product.price,
+    });
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de la récupération du produit.' });
   }
 };
+
+module.exports = getProductById;
+
 
 module.exports = getProductById;
