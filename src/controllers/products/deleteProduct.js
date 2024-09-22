66096ff398dc2c2d04d62e6932dab3c150ab6a0e
Precious <precious.soriano@ynov.com>
2024-09-22 14:59:51 +0200

    add: delete one or many products

--- src/controllers/products/deleteProduct.js
+++ src/controllers/products/deleteProduct.js
@@ -1,9 +1,22 @@
 const Product = require('../../models/productsModel');
 
 const deleteProduct = async (req, res) => {
-  const { id } = req.params;
-  
+  const { id } = req.params; // Utilisé pour la suppression d'un seul produit
+  const { ids } = req.body; // Utilisé pour la suppression multiple
+
   try {
+    // Si un tableau d'IDs est fourni, on effectue une suppression multiple
+    if (ids && Array.isArray(ids)) {
+      const deletedProducts = await Product.deleteMany({ _id: { $in: ids } });
+      
+      if (deletedProducts.deletedCount === 0) {
+        return res.status(404).json({ error: 'Aucun produit trouvé pour suppression.' });
+      }
+
+      return res.status(200).json({ message: `${deletedProducts.deletedCount} produits supprimés avec succès.` });
+    }
+    
+    // Sinon, on effectue une suppression d'un seul produit
     const deletedProduct = await Product.findByIdAndDelete(id);
     
     if (!deletedProduct) {
